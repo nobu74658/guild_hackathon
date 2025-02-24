39ccf0af07f2569aca38ea0d0ac40f8029215681
MATSUURA REI <sc24981i@st.omu.ac.jp>
2025-02-24 11:32:16 +0900

    refs #12 [add]指定した色のみのドット絵を出力する機能を追加

--- image_server/lib/convert_photo_to_pixel_art.py
+++ image_server/lib/convert_photo_to_pixel_art.py
@@ -13,16 +13,13 @@ class ConvertPhotoToPixelArt:
         self.image = image
         self.output_width = output_width
         self.output_height = output_height
-        # self.available_colors = available_colors
         self.available_colors = available_colors or [
-            (0, 0, 0),  # 黒(#000000)
-            (255, 255, 255), # 白(#ffffff)
-            (65, 105, 225), # 青(#4169e1)
-            (225, 225, 0)  # 黄色(#ffff00)
+            (0, 0, 0),
+            (255, 255, 255),
         ]
     
+    
     def closest_color(self, pixel):
-        """ 指定された色リストの中で、最も近い色を見つける """
         pixel = np.array(pixel)
         avalilable_colors = np.array(self.available_colors)
         distances = np.sqrt(np.sum((avalilable_colors - pixel) ** 2, axis=1))
@@ -33,20 +30,8 @@ class ConvertPhotoToPixelArt:
         image = self.image.resize(
             (self.output_width, self.output_height), resample=Image.NEAREST
         )
-
-        # グレースケール化
-        # image = image.convert("L")
-
-        # 画像をRGBモードに変換
         image = image.convert("RGB")
         pixels = np.array(image)
-
-        height, width, _ = pixels.shape
-        for y in range(height):
-            for x in range(width):
-                pixels[y, x] = self.closest_color(pixels[y, x])
-        
-        # 変換後の画像を作成
-        new_image = Image.fromarray(pixels)
+        pixels = np.apply_along_axis(self.closest_color, axis=2, arr=pixels)
+        new_image = Image.fromarray(pixels.astype(np.uint8))
         return new_image
-    
