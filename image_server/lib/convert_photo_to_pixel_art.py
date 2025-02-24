55befe538233c7c88c0a17bb41cf0a3f92bef82e
MATSUURA REI <sc24981i@st.omu.ac.jp>
2025-02-24 10:32:10 +0900

    refs #12 [add]コード内で指定した色のみのドット絵を出力する機能を追加

--- image_server/lib/convert_photo_to_pixel_art.py
+++ image_server/lib/convert_photo_to_pixel_art.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy as np
 
 
 class ConvertPhotoToPixelArt:
@@ -7,15 +8,45 @@ class ConvertPhotoToPixelArt:
         image: Image,
         output_width: int,
         output_height: int,
-        available_colors: list,
+        available_colors: list = None,
     ):
         self.image = image
         self.output_width = output_width
         self.output_height = output_height
-        self.available_colors = available_colors
+        # self.available_colors = available_colors
+        self.available_colors = available_colors or [
+            (0, 0, 0),  # 黒(#000000)
+            (255, 255, 255), # 白(#ffffff)
+            (65, 105, 225), # 青(#4169e1)
+            (225, 225, 0)  # 黄色(#ffff00)
+        ]
+    
+    def closest_color(self, pixel):
+        """ 指定された色リストの中で、最も近い色を見つける """
+        pixel = np.array(pixel)
+        avalilable_colors = np.array(self.available_colors)
+        distances = np.sqrt(np.sum((avalilable_colors - pixel) ** 2, axis=1))
+        return tuple(avalilable_colors[np.argmin(distances)])
+
 
     def convert(self):
         image = self.image.resize(
             (self.output_width, self.output_height), resample=Image.NEAREST
         )
-        return image
+
+        # グレースケール化
+        # image = image.convert("L")
+
+        # 画像をRGBモードに変換
+        image = image.convert("RGB")
+        pixels = np.array(image)
+
+        height, width, _ = pixels.shape
+        for y in range(height):
+            for x in range(width):
+                pixels[y, x] = self.closest_color(pixels[y, x])
+        
+        # 変換後の画像を作成
+        new_image = Image.fromarray(pixels)
+        return new_image
+    
