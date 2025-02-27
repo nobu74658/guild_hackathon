9013c13f1e493d549c22d75613db74c2bc79ceac
tanakacha <kntasihc20.18@gmail.com>
2025-02-27 14:59:18 +0900

    feature(Flutter): スライドバーの追加 #25

--- flutter_client/lib/view/knitting_pattern/knitting_pattern_screen.dart
+++ flutter_client/lib/view/knitting_pattern/knitting_pattern_screen.dart
@@ -19,14 +19,12 @@ class KnittingPatternScreen extends HookWidget {
       Colors.white
     ];
     final color = useState(paletteColors[0]);
-
-    // final color1 = useState(paletteColors[0]);
+    final scale = useValueNotifier<double>(1.0); // useValueNotifier に変更
 
     return Scaffold(
       key: scaffoldKey,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        // title: Text('arrow'),
         actions: [
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
@@ -37,258 +35,133 @@ class KnittingPatternScreen extends HookWidget {
                   icon: Icon(CupertinoIcons.arrow_turn_up_left),
                 ),
                 IconButton(
-                    onPressed: () {},
-                    icon: Icon(CupertinoIcons.arrow_turn_up_right))
+                  onPressed: () {},
+                  icon: Icon(CupertinoIcons.arrow_turn_up_right),
+                ),
               ],
             ),
           )
         ],
       ),
-      bottomNavigationBar: Builder(
-        builder: (scaffoldContext) => Container(
-          padding: EdgeInsets.only(
-            bottom: MediaQuery.of(scaffoldContext).viewPadding.bottom,
-          ),
-          color: Colors.white,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              GestureDetector(
-                onTap: () => showBottomSheet(
-                  context: scaffoldContext, // Scaffoldのcontextを取得
-                  builder: (context) => Container(
-                    height: 200,
-                    width: double.infinity,
-                    color: Colors.white,
-                    // MediaQuery.of(context).size.height * 0.5, // 高さを調整
-                    // color: Colors.white.withOpacity(0.8), // 半透明にする
-                    child: Column(
-                      // mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Padding(
-                              padding: EdgeInsets.all(5),
-                              child: Text(
-                                // 登録済みのパレットから選択中のものを表示したい(「パレット1」とか「コットンヤーン(毛糸の種類)」とか)けど、急ぎじゃない
-                                'パレット',
-                                style: TextStyle(
-                                  // width:2,
-                                  // height: 2,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey,
-                                ),
+      bottomNavigationBar: SizedBox(
+        height: 150,
+        child: Builder(
+          builder: (scaffoldContext) => Container(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(scaffoldContext).viewPadding.bottom,
+            ),
+            color: Colors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: () => showBottomSheet(
+                        context: scaffoldContext,
+                        builder: (context) => Container(
+                          height: 200,
+                          width: double.infinity,
+                          color: Colors.white,
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Padding(
+                                    padding: EdgeInsets.all(5),
+                                    child: Text(
+                                      'パレット',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ),
+                                  Align(
+                                    alignment: Alignment.topRight,
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.white,
+                                        elevation: 0,
+                                      ),
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Icon(
+                                          IconData(0xf645,
+                                              fontFamily: 'MaterialIcons'),
+                                          color: Colors.black),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                            Align(
-                              alignment: Alignment.topRight,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.white,
-                                  elevation: 0,
-                                ),
-                                onPressed: () =>
-                                    Navigator.pop(context), // ボトムシートを閉じる
-                                child: const Icon(
-                                    IconData(0xf645,
-                                        fontFamily: 'MaterialIcons'),
-                                    color: Colors.black),
-                              ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                        // 色塗りのパレットの色選択
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                GestureDetector(
-                                  onTap: () {
-                                    color.value = paletteColors[0];
-                                  },
-                                  child: Container(
-                                    width: 50,
-                                    height: 50,
-                                    // padding: const EdgeInsets.symmetric(
-                                    // horizontal: 10),
-                                    margin: const EdgeInsets.all(5),
-                                    alignment: Alignment.topLeft,
-                                    decoration: BoxDecoration(
-                                      color: paletteColors[0],
-                                      border: Border.all(
-                                        color: (paletteColors[0] == Colors.white
-                                            ? Colors.grey
-                                            : paletteColors[0]),
-                                        width: 1,
-                                      ),
-                                      // borderRadius: BorderRadius.circular(2),
-                                      shape: BoxShape.circle,
-                                    ),
-                                  ),
-                                ),
-                                GestureDetector(
-                                  onTap: () {
-                                    color.value = paletteColors[1];
-                                  },
-                                  child: Container(
-                                    width: 50,
-                                    height: 50,
-                                    // padding: const EdgeInsets.symmetric(
-                                    // horizontal: 10),
-                                    margin: const EdgeInsets.all(5),
-                                    alignment: Alignment.topLeft,
-                                    decoration: BoxDecoration(
-                                      color: paletteColors[1],
-                                      border: Border.all(
-                                        color: (paletteColors[1] == Colors.white
-                                            ? Colors.grey
-                                            : paletteColors[1]),
-                                        width: 1,
-                                      ),
-                                      // borderRadius: BorderRadius.circular(2),
-                                      shape: BoxShape.circle,
-                                    ),
-                                  ),
-                                ),
-                                GestureDetector(
-                                  onTap: () {
-                                    color.value = paletteColors[2];
-                                  },
-                                  child: Container(
-                                    width: 50,
-                                    height: 50,
-                                    // padding: const EdgeInsets.symmetric(
-                                    // horizontal: 10),
-                                    margin: const EdgeInsets.all(5),
-                                    alignment: Alignment.topLeft,
-                                    decoration: BoxDecoration(
-                                      color: paletteColors[2],
-                                      border: Border.all(
-                                        color: (paletteColors[2] == Colors.white
-                                            ? Colors.grey
-                                            : paletteColors[2]),
-                                        width: 1,
-                                      ),
-                                      // borderRadius: BorderRadius.circular(2),
-                                      shape: BoxShape.circle,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                GestureDetector(
-                                  onTap: () {
-                                    color.value = paletteColors[3];
-                                  },
-                                  child: Container(
-                                    width: 50,
-                                    height: 50,
-                                    // padding: const EdgeInsets.symmetric(
-                                    // horizontal: 10),
-                                    margin: const EdgeInsets.all(5),
-                                    alignment: Alignment.topLeft,
-                                    decoration: BoxDecoration(
-                                      color: paletteColors[3],
-                                      border: Border.all(
-                                        color: (paletteColors[3] == Colors.white
-                                            ? Colors.grey
-                                            : paletteColors[3]),
-                                        width: 1,
-                                      ),
-                                      // borderRadius: BorderRadius.circular(2),
-                                      shape: BoxShape.circle,
-                                    ),
-                                  ),
-                                ),
-                                GestureDetector(
-                                  onTap: () {
-                                    color.value = paletteColors[4];
-                                  },
-                                  child: Container(
-                                    width: 50,
-                                    height: 50,
-                                    // padding: const EdgeInsets.symmetric(
-                                    // horizontal: 10),
-                                    margin: const EdgeInsets.all(5),
-                                    alignment: Alignment.topLeft,
-                                    decoration: BoxDecoration(
-                                      color: paletteColors[4],
-                                      border: Border.all(
-                                        color: (paletteColors[4] == Colors.white
-                                            ? Colors.grey
-                                            : paletteColors[4]),
-                                        width: 1,
-                                      ),
-                                      // borderRadius: BorderRadius.circular(2),
-                                      shape: BoxShape.circle,
-                                    ),
-                                  ),
-                                ),
-                                GestureDetector(
-                                  onTap: () {
-                                    color.value = paletteColors[5];
-                                  },
-                                  child: Container(
-                                    width: 50,
-                                    height: 50,
-                                    // padding: const EdgeInsets.symmetric(
-                                    // horizontal: 10),
-                                    margin: const EdgeInsets.all(5),
-                                    alignment: Alignment.topLeft,
-                                    decoration: BoxDecoration(
-                                      color: paletteColors[5],
-                                      border: Border.all(
-                                        color: (paletteColors[5] == Colors.white
-                                            ? Colors.grey
-                                            : paletteColors[5]),
-                                        width: 1,
-                                      ),
-                                      // borderRadius: BorderRadius.circular(2),
-                                      shape: BoxShape.circle,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
+                      ),
+                      child: Container(
+                        width: 25,
+                        height: 25,
+                        decoration: BoxDecoration(
+                          color: color.value,
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(2),
                         ),
-                      ],
+                      ),
                     ),
-                  ),
+                    //   ElevatedButton(
+                    //     onPressed: () {
+                    //       if (scale.value < 2.0) {
+                    //         scale.value += 0.1;
+                    //       }
+                    //     },
+                    //     child: const Text('Zoom In'),
+                    //   ),
+                    //   ElevatedButton(
+                    //     onPressed: () {
+                    //       if (scale.value > 0.5) {
+                    //         scale.value -= 0.1;
+                    //       }
+                    //     },
+                    //     child: const Text('Zoom Out'),
+                    //   ),
+                  ],
                 ),
-                child: Container(
-                  width: 25,
-                  height: 25,
-                  decoration: BoxDecoration(
-                    // 選択中の色を表示
-                    // color: Colors.pink[50],
-                    color: color.value,
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(2),
-                  ),
+                ValueListenableBuilder<double>(
+                  valueListenable: scale,
+                  builder: (context, value, child) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Slider(
+                        value: value,
+                        min: 1,
+                        max: 8,
+                        // スライドしてる位置の値が表示される、無いものを想定してたけどあっても便利かも？
+                        label: value.toStringAsFixed(1),
+                        onChanged: (newValue) {
+                          scale.value = newValue; // スライダーの値を更新
+                        },
+                      ),
+                    );
+                  },
                 ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Zoom In'),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Zoom Out'),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
       body: LayoutBuilder(
-        builder: (context, constraints) => ConnectedKnittingPatternViewer(
-          maxHeight: constraints.maxHeight,
+        builder: (context, constraints) => ValueListenableBuilder<double>(
+          valueListenable: scale,
+          builder: (context, value, child) {
+            return Transform.scale(
+              scale: value,
+              child: ConnectedKnittingPatternViewer(
+                maxHeight: constraints.maxHeight,
+              ),
+            );
+          },
         ),
       ),
     );
