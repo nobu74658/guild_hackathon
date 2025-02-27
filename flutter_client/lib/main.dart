d0bbba1559c5da63df6072bbb78c183af75466b1
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2025-02-27 14:19:52 +0900

    feat(flutter_client): Go Routerの実装

--- flutter_client/lib/main.dart
+++ flutter_client/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:knitting/account/authentication.dart';
+import 'package:knitting/common/router.dart';
 import 'package:knitting/common/theme.dart';
 import 'package:knitting/firebase_options.dart';
 import 'package:knitting/view/knitting_pattern/knitting_pattern_screen.dart';
@@ -26,9 +27,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: AppTheme.get,
-      home: const KnittingPatternScreen(),
+      routerConfig: router,
     );
   }
 }
