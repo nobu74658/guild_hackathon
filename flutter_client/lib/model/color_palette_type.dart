a0cbf5fe2ecb30bc77103e491284ca2a43e637de
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2025-03-01 12:47:04 +0900

    fix(flutter_client): デモ用の色を追加

---
+++ flutter_client/lib/model/color_palette_type.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+enum ColorPaletteType {
+  first('パレット1', [
+    Colors.white,
+    Colors.grey,
+    Colors.blueAccent,
+    Colors.black,
+  ]),
+  second('パレット1', [
+    Colors.green,
+    Colors.orange,
+    Colors.pink,
+    Colors.brown,
+    Colors.black,
+    Colors.teal,
+  ]),
+  third('パレット2', [
+    Colors.yellow,
+    Colors.red,
+    Colors.blue,
+    Colors.purple,
+    Colors.grey,
+    Colors.white,
+  ]),
+  ;
+
+  const ColorPaletteType(this.label, this.paletteColors);
+
+  final String label;
+  final List<Color> paletteColors;
+}
