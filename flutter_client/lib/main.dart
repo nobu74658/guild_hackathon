b2e9735529f90f0b4156b46f0e11fe6af9917d63
nobu74658 <hiroki-pon@outlook.jp>
2025-03-02 14:52:01 +0900

    feature(Flutter): デバッグラベルを削除

--- flutter_client/lib/main.dart
+++ flutter_client/lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.get,
       routerConfig: router,
     );
