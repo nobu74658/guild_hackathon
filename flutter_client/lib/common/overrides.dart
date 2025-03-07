29fda14d5544dd555a1b2e682d9ecb24a5ed7788
nobu74658 <hiroki-pon@outlook.jp>
2025-03-08 00:40:29 +0900

    feature(Flutter): 環境の切り替え #98

--- flutter_client/lib/common/overrides.dart
+++ flutter_client/lib/common/overrides.dart
@@ -1,13 +1,27 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:knitting/infra/fake_project_repository.dart';
+import 'package:knitting/infra/project_fast_api_repository.dart';
 import 'package:knitting/infra/project_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 class Overrides {
-  static List<Override> value =
-      const bool.fromEnvironment('isFake') ? _fakeInfra : [];
+  static String get _env => const String.fromEnvironment('flavor');
+  static List<Override> get value {
+    switch (_env) {
+      case 'dev':
+        return _devInfra;
+      case 'stg':
+        return _stgInfra;
+      default:
+        return [];
+    }
+  }
 
-  static final List<Override> _fakeInfra = [
+  static final List<Override> _devInfra = [
     projectRepositoryProvider.overrideWithValue(FakeProjectRepository()),
+  ];
+
+  static final List<Override> _stgInfra = [
+    projectRepositoryProvider.overrideWithValue(ProjectFastApiRepository()),
   ];
 }
