e457e82b5f6a0a95d8c0621dfff46c8463045eed
nobu74658 <hiroki-pon@outlook.jp>
2025-03-08 00:55:11 +0900

    feature(Flutter): fastApiと接続完了 #98

--- flutter_client/lib/infra/project_fast_api_repository.dart
+++ flutter_client/lib/infra/project_fast_api_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 import 'package:image/image.dart' as img;
@@ -43,19 +44,27 @@ class ProjectFastApiRepository extends ProjectRepositoryInterface {
     List<String> colors,
   ) async {
     try {
+      final colorHexes = colors
+          .map(
+            (color) => '&available_colors_hex=$color'.replaceFirst('#', '%23'),
+          )
+          .join();
       final url = Uri.parse(
-        'https://knitting-image-server-jlgdobzt4q-an.a.run.app/api/convert?output_column=100&output_row=100&width_ratio=1&height_ratio=1&shear=0&available_colors_hex=%23000000&available_colors_hex=%23ffffff',
+        'https://knitting-image-server-jlgdobzt4q-an.a.run.app/api/convert?output_column=$width&output_row=$height&width_ratio=1&height_ratio=1&shear=0$colorHexes',
       );
+
+      final encodedImage = Uint8List.fromList(img.encodePng(image));
 
       final request = http.MultipartRequest('POST', url)
         ..headers.addAll({
           'accept': 'application/json',
-          'Content-Type': 'image/png',
+          'Content-Type': 'multipart/form-data',
         })
         ..files.add(
           http.MultipartFile.fromBytes(
             'image_file',
-            img.encodePng(image),
+            encodedImage,
+            filename: 'image.png',
             contentType: MediaType.parse('image/png'),
           ),
         );
@@ -68,9 +77,7 @@ class ProjectFastApiRepository extends ProjectRepositoryInterface {
       }
       final responseBytes = await response.stream.toBytes();
 
-      print('responseBytes: $responseBytes');
       final uint8List = Uint8List.fromList(responseBytes);
-      print('uint8List: $uint8List');
 
       final responseImage = img.decodePng(uint8List);
       if (responseImage == null) {
@@ -78,7 +85,7 @@ class ProjectFastApiRepository extends ProjectRepositoryInterface {
       }
       return responseImage;
     } catch (e) {
-      print('Failed to connect to the server. Error: $e');
+      debugPrint('Failed to connect to the server. Error: $e');
       throw Exception('Failed to connect to the server. Error: $e');
     }
   }
